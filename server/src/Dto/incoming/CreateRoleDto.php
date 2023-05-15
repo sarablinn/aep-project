eec1c376d079fe87d89026082ba21ca5d3122f0c
sarablinn <sblinn@kipsu.com>
2023-05-15 16:03:44 -0500

    dtos created

---
+++ server/src/Dto/incoming/CreateRoleDto.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Dto\incoming;
+
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\Constraints\Type;
+
+class CreateRoleDto
+{
+
+    #[NotNull]
+    #[Type('string')]
+    private string $role_name;
+
+
+    /**
+     * @return string
+     */
+    public function getRoleName(): string
+    {
+        return $this->role_name;
+    }
+
+    /**
+     * @param string $role_name
+     */
+    public function setRoleName(string $role_name): void
+    {
+        $this->role_name = $role_name;
+    }
+
+}
---
+++ server/src/Dto/outgoing/RoleDto.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Dto\outgoing;
+
+class RoleDto
+{
+    #TODO should these RoleDto fields have the option of being null?
+    private int $role_id;
+    private string $role_name;
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getRoleId(): int
+    {
+        return $this->role_id;
+    }
+
+    /**
+     * @param int $role_id
+     */
+    public function setRoleId(int $role_id): void
+    {
+        $this->role_id = $role_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRoleName(): string
+    {
+        return $this->role_name;
+    }
+
+    /**
+     * @param string $role_name
+     */
+    public function setRoleName(string $role_name): void
+    {
+        $this->role_name = $role_name;
+    }
+
+}
