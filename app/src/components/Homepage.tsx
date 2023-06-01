e728d91a852056f97f17fbddf25c548bfff869cc
sarablinn <sblinn@kipsu.com>
2023-06-01 14:16:42 -0500

    Update Homepage.tsx

--- app/src/components/Homepage.tsx
+++ app/src/components/Homepage.tsx
@@ -4,34 +4,6 @@ import { useAtom } from 'jotai';
 const Homepage = () => {
   const [user] = useAtom(selectedUser);
 
-  // const {
-  //   isLoading: countriesAreLoading,
-  //   error: countriesError,
-  //   data: countriesData,
-  // } = useQuery({
-  //   queryKey: [`countries`],
-  //   queryFn: () => getCountries(),
-  // });
-  //
-  // if (countriesAreLoading)
-  //   return (
-  //     <div>
-  //       <Loading />
-  //     </div>
-  //   );
-  //
-  // if (countriesError) return <Error />;
-  //
-  // if (countriesData) {
-  //   if (!country) {
-  //     setCountry(countriesData[0]);
-  //   }
-  //
-  //   //sorts alphabetically
-  //   countriesData.sort((a: countryResource, b: countryResource) =>
-  //     a.name.localeCompare(b.name),
-  //   );
-
   return (
     <div
       className={`grid grid-cols-4 py-2`}
