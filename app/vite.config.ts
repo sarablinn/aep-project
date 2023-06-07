93ec8b36c4ff9aff6bff7aa2e9ff71a28fc20e09
sarablinn <sblinn@kipsu.com>
2023-06-07 13:43:25 -0500

    update details

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,9 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
-
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-})
+  server: {
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['../..'],
+    },
+  },
+});
