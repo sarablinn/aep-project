8451e5bdaedecd070ffdacae82b94fdac3b9cf40
sarablinn <sblinn@kipsu.com>
2023-06-15 09:25:22 -0500

    removed console logging

--- app/src/hooks/useInput.ts
+++ app/src/hooks/useInput.ts
@@ -9,17 +9,17 @@ const useInput = (validateInput, startingValue: string) => {
   const [isFocused, setToFocused] = useState(false);
 
   const setStartValue = () => {
-    console.log('Starting value is set to ' + startingValue);
+    // console.log('Starting value is set to ' + startingValue);
     setEnteredValue(startingValue);
   };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    console.log('Input change is being handled: enteredValue: ' + enteredValue);
+    // console.log('Input change is being handled: enteredValue: ' + enteredValue);
     setEnteredValue(event.target.value);
   };
 
   const setIsFocused = () => {
-    console.log('Element has been set to focus.');
+    // console.log('Element has been set to focus.');
     setToFocused(true);
   };
 
@@ -30,17 +30,17 @@ const useInput = (validateInput, startingValue: string) => {
 
   useEffect(() => {
     setErrorMessage(validateInput(initialValue, enteredValue));
-    console.log(
-      'Input Validation useEffect: enteredValue: ' +
-        enteredValue +
-        ': error message set to: ' +
-        errorMessage,
-    );
+    // console.log(
+    //   'Input Validation useEffect: enteredValue: ' +
+    //     enteredValue +
+    //     ': error message set to: ' +
+    //     errorMessage,
+    // );
   }, [enteredValue, validateInput]);
 
   useEffect(() => {
     setIsValid(errorMessage == '');
-    console.log('IsValid set to: ' + isValid);
+    // console.log('IsValid set to: ' + isValid);
   }, [validateInput]);
 
   return {
