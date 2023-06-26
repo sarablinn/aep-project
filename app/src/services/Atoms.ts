e14aa5ebdad02828fb8e66675c3c963b34083a45
sarablinn <sblinn@kipsu.com>
2023-06-26 13:16:30 -0500

    removed gameGrid atom

--- app/src/services/Atoms.ts
+++ app/src/services/Atoms.ts
@@ -1,6 +1,7 @@
 import { atom } from 'jotai';
 import { UserResource } from './userApi';
 import { Role } from './roleApi';
+import { GameGrid } from './gameApi';
 
 const defaultUser = {
   userId: 0,
