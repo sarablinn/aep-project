a538ce97a8ce4f59e9c265bb61bcdbb3fd5b3708
sarablinn <sblinn@kipsu.com>
2023-06-29 13:26:32 -0500

    added div

--- app/src/components/Homepage.tsx
+++ app/src/components/Homepage.tsx
@@ -5,13 +5,15 @@ const Homepage = () => {
   const [user] = useAtom(selectedUser);
 
   return (
-    <div
-      className={`grid grid-cols-4 py-2`}
-      style={{
-        backgroundColor: user.foregroundColor,
-        color: user.backgroundColor,
-      }}
-    ></div>
+    <div>
+      <div
+        className={`grid grid-cols-4 py-2`}
+        style={{
+          backgroundColor: user.foregroundColor,
+          color: user.backgroundColor,
+        }}
+      ></div>
+    </div>
   );
 };
 
