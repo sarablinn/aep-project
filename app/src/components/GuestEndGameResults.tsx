5feb7dc89617308edb3f7ff41d532504ab3e1b1f
sarablinn <sblinn@kipsu.com>
2023-07-07 09:10:37 -0500

    update format

--- app/src/components/GuestEndGameResults.tsx
+++ app/src/components/GuestEndGameResults.tsx
@@ -4,10 +4,10 @@ export type GuestEndGameResultsProps = {
 
 const GuestEndGameResults = ({ score }: GuestEndGameResultsProps) => {
   return (
-    <div>
-      <h2>You Died.</h2>
-      <h1>Final Score</h1>
-      <h1>{score}</h1>
+    <div className="container flex flex-col items-center p-10">
+      <h2 className="p-5 font-bold text-white">You Died.</h2>
+      <h1 className="font-bold text-white">Final Score</h1>
+      <h1 className="font-bold text-white">{score}</h1>
     </div>
   );
 };
--- app/src/components/UserEndGameResults.tsx
+++ app/src/components/UserEndGameResults.tsx
@@ -7,10 +7,10 @@ export type UserEndGameResultsProps = {
 
 const UserEndGameResults = ({ user, game }: UserEndGameResultsProps) => {
   return (
-    <div>
-      <h2>You Died.</h2>
-      <h1>Final Score</h1>
-      <h1>{game.score}</h1>
+    <div className="container flex flex-col items-center p-10">
+      <h2 className="p-5 font-bold text-white">You Died.</h2>
+      <h1 className="font-bold text-white">Final Score</h1>
+      <h1 className="font-bold text-white">{game.score}</h1>
     </div>
   );
 };
